61eb09ced997dd4dc19dc8325a6bbdffb53f81cb
jesus Elias Maza Gomez <144734211+scaraft46@users.noreply.github.com>
2023-12-06 11:47:42 -0500

    Create librerias.sh

---
+++ 18 bash scrip/librerias.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# No es que existan las librerías pero se puede simular
+# algo similar
+# Esta es la forma de importar funciones
+source libreria.sh
+muestrapantalla 69 123 "epa"
+sumame 1337 3389 && echo "OK" || echo "Ocurrió un error"
+echo "Resultado: ${RESULTADO} $!"}
